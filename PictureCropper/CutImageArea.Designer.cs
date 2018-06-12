1a54770562fef37d5d3f6bc10120547a6ac009de
sergeyg gaan <sergeygaan@mail.ru>
2018-06-12 22:31:01 +0700

    Добавлен пример плохих фото

--- PictureCropper/CutImageArea.Designer.cs
+++ PictureCropper/CutImageArea.Designer.cs
@@ -124,7 +124,7 @@
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(473, 145);
+            this.button5.Location = new System.Drawing.Point(470, 145);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(136, 23);
             this.button5.TabIndex = 10;
